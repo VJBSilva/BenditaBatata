f94759c5375b23f6a69d419d63bc5290ef91a3f7
VJBSilva <valdecyjunior@hotmail.com>
2025-03-17 16:44:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -237,7 +237,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 gap: 10px;
             }
             .form-group textarea {
-                width: 70%; /* Largura reduzida pela metade */
+                width: 100%; /* Largura reduzida pela metade */
             }
             .form-group input[type="text"] {
                 width: 30%; /* Largura reduzida pela metade */
