e2b6f241282c8256ffed90d09c3c85e9b864a8ac
VJBSilva <valdecyjunior@hotmail.com>
2025-03-17 20:41:00 -0300

    Update excluir_pedido.php

--- excluir_pedido.php
+++ excluir_pedido.php
@@ -1,6 +1,12 @@
 <?php
 require 'conexao.php';
 
+// Verifica se o usuário está logado e é um administrador
+if (!isset($_COOKIE['usuario_id']) || $_COOKIE['tipo_usuario'] !== 'admin') {
+    header("Location: index.php");
+    exit();
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
     $pedidoId = $data['id'];
